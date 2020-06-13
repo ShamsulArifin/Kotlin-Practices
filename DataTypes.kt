5a143cda80a898675f4b381ff257f66c2c863b08
Shamsul Arifin <52891680+ShamsulArifin@users.noreply.github.com>
2020-06-13 20:56:39 +0600

    Add files via upload

---
+++ DataTypes.kt
@@ -0,0 +1,13 @@
+
+fun main(args:Array<String>) {
+    var name= "Arifin"
+    var age= 24
+
+    var dep:String?
+    dep="Software Engineering"
+
+    println("name:"+ name)
+    println("age:"+ age)
+    println("department:"+ dep)
+
+}
